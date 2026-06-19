3722b0a22661ed9db96027f93026c35c88e34529
mclowam <max.shabelnikov@gmail.com>
2026-06-20 01:34:17 +0500

    made library abc

--- src/library_service/domain/abstractions/library.py
+++ src/library_service/domain/abstractions/library.py
@@ -1,14 +1,19 @@
 import uuid
 from typing import Protocol, runtime_checkable
 
+from src.library_service.domain.entities.library import LibraryCreateSchema
+
 
 @runtime_checkable
 class ILibraryRepository(Protocol):
+    async def save(self, obj: LibraryCreateSchema):
+        pass
+
     async def get_by_id(self, obj_id: uuid.UUID):
         pass
 
-    async def add(self, obj):
+    async def delete(self, obj_id: uuid.UUID, user_id: uuid.UUID):
         pass
 
-    async def remove(self, obj_id: uuid.UUID):
+    async def list_by_user(self, user_id: uuid.UUID):
         pass
