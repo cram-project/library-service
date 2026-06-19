f3badcafc4cb90312af89cbe020837e027b8db6a
mclowam <max.shabelnikov@gmail.com>
2026-06-20 01:04:45 +0500

    made a document storage

--- src/library_service/infrastructure/storage/document_storage.py
+++ src/library_service/infrastructure/storage/document_storage.py
@@ -1,4 +1,4 @@
-from src.library_service.config import Settings
+from src.library_service.config import Settings, session_minio
 
 
 class DocumentStorage:
@@ -9,6 +9,37 @@ class DocumentStorage:
             self,
             file_obj,
             key: str,
-
+            content_type: str | None = None,
     ):
-        pass
+        extra: dict[str, str] = {}
+        if content_type:
+            extra["ContentType"] = content_type
+
+        async with session_minio.client(
+                "s3",
+                endpoint_url=self._settings.MINIO_ENDPOINT,
+                aws_access_key_id=self._settings.MINIO_ACCESS_KEY,
+                aws_secret_access_key=self._settings.MINIO_SECRET_KEY,
+        ) as s3:
+            bucket = self._settings.MINIO_BUCKET
+            if extra:
+                await s3.upload_fileobj(file_obj, bucket, key, ExtraArgs=extra)
+            else:
+                await s3.upload_fileobj(file_obj, bucket, key)
+
+    async def get_bytes(self, key: str) -> tuple[bytes, str]:
+        async with session_minio.client(
+                "s3",
+                endpoint_url=self._settings.MINIO_ENDPOINT,
+                aws_access_key_id=self._settings.MINIO_ACCESS_KEY,
+                aws_secret_access_key=self._settings.MINIO_SECRET_KEY,
+        ) as s3:
+            response = await s3.get_object(
+                Bucket=self._settings.MINIO_BUCKET,
+                Key=key,
+            )
+            data = await response["Body"].read()
+
+            content_type = response.get("ContentType", "application/octet-stream")
+
+            return data, content_type
